68ca00a59854b9d97a9c9eb2bb9f7fce0c4ca7b0
contatomauricio <contatomauricio@hotmail.com>
2023-08-16 19:46:36 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import openai
 
@@ -15,19 +15,25 @@ def terapeuta_resposta(pergunta, historico):
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
-        max_tokens=150,  # Ajuste este valor conforme necessário
-        temperature=0.7  # Ajuste a criatividade da resposta (0.2 - 1.0)
+        max_tokens=150,
+        temperature=0.7
     )
     resposta = response.choices[0].text.strip()
     return resposta
 
-@app.route('/chatbot')  
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/chatbot', methods=['POST'])
 def terapeuta():
-    pergunta = request.args.get('pergunta')
+    pergunta = request.form['pergunta']
     
     resposta_terapeuta = terapeuta_resposta(pergunta, historico)
     historico.append(f"Usuário: {pergunta}\nElisa: {resposta_terapeuta}")
     
     return jsonify(resposta=resposta_terapeuta)
 
-app.run()
+if __name__ == '__main__':
+    app.run()
+
